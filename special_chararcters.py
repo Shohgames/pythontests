9dfc25018e9e9056f76db25a90a0eefed59d79ba
GayLordBanana21241 <sjv22001@student.mdu.se>
2025-08-11 06:59:36 +0900

    finished with special chars

---
+++ special_chararcters.py
@@ -0,0 +1,29 @@
+
+
+print("Special characters in Python")
+
+print("\n1. NEWLINE:")
+print("this is \\n")
+print("\n")
+print("it makes a newline like that space above")
+
+print("\n2. CARRIAGE RETURN")
+print("this is \\r")
+print("it returns the cursor back to the start of the line")
+print("hello \rworld")
+print("it writes hello at first, but then moves the cursor to the start, and writes on top")
+
+print("\n3. TAB")
+print("this is \\t")
+print("it adds a horizontal tab space")
+print("\thello world")
+
+print("\n4. BACKSPACE")
+print("this is \\b")
+print("it moves the cursor one character backward")
+print("hello\bworld")
+
+print("\n5.FORM FEED")
+print("this is \\f")
+print("it enters into a new line, but its not used that much today.")
+print("Hello\fWorld")
